3c6c75a7c734976347213de5d4b5ca95717444c2
alexnasyr <alexnasyr@gmail.com>
2021-09-09 07:33:45 +0300

    Добавьте файлы проекта.

---
+++ MShapes.Tests/UnitTest1.cs
@@ -0,0 +1,11 @@
+using NUnit.Framework;
+
+namespace MShapes.Tests {
+    [TestFixture]
+    public class MShapesCircleTests {
+    
+        [TestMethod]
+        public void TestMethod1() {
+        }
+    }
+}
---
+++ MShapes/Circle.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace MShapes {
+    public class Circle : ISquareable {
+        /// <summary>
+        /// Implements the Circle item with square calculating
+        /// </summary>
+        /// <param name="dimensions">ineger array from one item. represents the circle radius.</param>
+        /// <returns>
+        /// function "Square" returns double value. represents the objects square.
+        /// </returns>
+        /// <exception cref="ArgumentException">Thrown when wrong parameters passed to constructor.</exception>
+        public int[] Dimensions { get; set; }
+        public Circle(int[] dimensions) {
+            if(dimensions.Length != 1) {
+                throw new ArgumentException($"wrong argument parameters {nameof(dimensions)}");
+            }
+            Dimensions = dimensions;
+        }
+        public double Square() => 3.14 * Dimensions[0] * Dimensions[0];
+    }
+}
